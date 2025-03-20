b1c52874dbf60d913eeb41d5ee92f5f121de0a48
Tylor <tylor.9.kid@gmail.com>
2025-03-20 17:28:47 +0300

    Add files via upload

--- lib/Types/GroupMetadata.d.ts
+++ lib/Types/GroupMetadata.d.ts
@@ -4,9 +4,7 @@ export type GroupParticipant = (Contact & {
     isSuperAdmin?: boolean;
     admin?: 'admin' | 'superadmin' | null;
 });
-export type ParticipantAction = 'add' | 'remove' | 'promote' | 'demote' | 'modify';
-export type RequestJoinAction = 'created' | 'revoked' | 'rejected';
-export type RequestJoinMethod = 'invite_link' | 'linked_group_join' | 'non_admin_add' | undefined;
+export type ParticipantAction = 'add' | 'remove' | 'promote' | 'demote';
 export interface GroupMetadata {
     id: string;
     owner: string | undefined;
